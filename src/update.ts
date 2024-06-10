01ebd5c95b0f9e822294f7bc58387cb9ac7bd151
CitybookServicesLTD <139445281+CitybookServicesLTD@users.noreply.github.com>
2024-06-10 15:41:39 +0300

    Update update.ts

--- src/update.ts
+++ src/update.ts
@@ -292,7 +292,7 @@ if (site.check === "tcp-ping") {
         console.log('ErrorCode: ' + error.code + ' IP: ' + error.address);
         if (error.code == "ETIMEDOUT" && error.address){
           console.log("Request timeout. but found IP: " + error.address);
-          var randomResponeTime = Math.floor(Math.random() * 1000);
+          var randomResponeTime = Math.floor(Math.random() * (30) + 10);
           return { result: { httpCode: 408 }, responseTime: (randomResponeTime).toFixed(0), status: "up" };
         } else {
         console.log("ERROR Got pinging error from async call", error);
